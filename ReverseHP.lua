72ae2d17d2ffa1e3dbca8bc3df48784984d58033
TheNja09 <98636004+TheNja09@users.noreply.github.com>
2022-12-20 20:11:49 -0600

    Update ReverseHP.lua

--- ReverseHP.lua
+++ ReverseHP.lua
@@ -42,7 +42,7 @@ function Cheats()
 		WriteByte(Slot1+0x0, 210)
 	end
 	if ReadByte(Slot1+0x4) == 212 or ReadByte(Slot1+0x4) == 202 or ReadByte(Slot1+0x4) == 192 or ReadByte(Slot1+0x4) == 182 or ReadByte(Slot1+0x4) == 172 or ReadByte(Slot1+0x4) == 162 or ReadByte(Slot1+0x4) == 152 or ReadByte(Slot1+0x4) == 142 or ReadByte(Slot1+0x4) == 132 or ReadByte(Slot1+0x4) == 122 or ReadByte(Slot1+0x4) == 112 or ReadByte(Slot1+0x4) == 102 or ReadByte(Slot1+0x4) == 92 or ReadByte(Slot1+0x4) == 82 or ReadByte(Slot1+0x4) == 72 or ReadByte(Slot1+0x4) == 62 or ReadByte(Slot1+0x4) == 52 or ReadByte(Slot1+0x4) == 42 or ReadByte(Slot1+0x4) == 32 or ReadByte(Slot1+0x4) == 22 then
-		WriteByte(Slot1+0x4 - 12)
+		WriteByte(Slot1+0x4, ReadByte(Slot1+0x4) - 12)
 	elseif ReadByte(Slot1+0x4) == 12 then 
 		WriteByte(Slot1+0x4, 10)
 	end
